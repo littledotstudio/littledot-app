289e0935ffe0c4bca6bdabb7c1941fa3611df4d9
Roy Hu <royhu@Roys-MBP.hsd1.wa.comcast.net>
2022-11-18 18:26:39 -0800

    blah

--- app.py
+++ app.py
@@ -1,34 +1,24 @@
 import streamlit as st
+import subprocess
+import sys
 
 
 def main():
-    df = load_data()
-    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
+    page = st.sidebar.selectbox("Choose a page", ["Move Approved Orders", "Exploration"])
 
-    if page == "Homepage":
-        st.header("This is your data explorer.")
-        st.write("Please select a page on the left.")
-        st.write(df)
-    elif page == "Exploration":
-        st.title("Data Exploration")
-        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
-        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
-        visualize_data(df, x_axis, y_axis)
-
-@st.cache
-def load_data():
-    df = data.cars()
-    return df
-
-def visualize_data(df, x_axis, y_axis):
-    graph = alt.Chart(df).mark_circle(size=60).encode(
-        x=x_axis,
-        y=y_axis,
-        color='Origin',
-        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
-    ).interactive()
-
-    st.write(graph)
+    if page == "Move Approved Orders":
+        st.header("This is approve order mover tool.")
+        st.write("This button will move things from 03_Files Approved for Processing to new batch folders in 01_Order Production")
+        st.write("Make sure the word 'batch' is in the folder name!")
+        result = st.button("Move Files!")
+        if result:
+            subprocess.run([f"{sys.executable}", "/app/gdrive_scripts/refresh_token.py"])
+            st.write("finished")
+    # elif page == "Exploration":
+    #     st.title("Data Exploration")
+    #     x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
+    #     y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
+    #     visualize_data(df, x_axis, y_axis)
 
 if __name__ == "__main__":
     main()
---
+++ gdrive_scripts/fetch_data.py
@@ -0,0 +1,76 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.cloud import bigquery
+
+bq = bigquery.Client()
+table0 = 'hubspot.deal'
+
+deal_table = bigquery.TableReference.from_string(
+    'radiant-rookery-274409.'+table0
+)
+
+rows = bq.list_rows(
+    deal_table,
+)
+# Upsert logic
+df_approved = rows.to_dataframe(
+    # Optionally, explicitly request to use the BigQuery Storage API. As of
+    # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
+    # API is used by default.
+    create_bqstorage_client=True,
+)
+
+stages = ['21063207','decisionmakerboughtin','contractsent']
+approved_ids = df_approved[df_approved['deal_pipeline_stage_id'].isin(stages)]['property_dealname'].values
+approved_ids2 = [x+'.pdf' for x in approved_ids]
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# Code 05_CodeTesting
+#file_id = '1KGSWoD6rbdbIm-Sl8JBxowKobHq21X0V'
+
+
+# Move
+#results = service.files().list(pageSize=10, q = "'1qn5SRq6-o81djmXqMRJYBt1MIJo_aWdI' in parents", fields="nextPageToken, files(id, name)").execute()
+
+#results = service.files().list(pageSize=10, q="name='02_Pending Customer Review'", fields="nextPageToken, files(id, name)").execute()
+#results = service.files().list(pageSize=10, q = "'1XU0nCVjQbFlK6bzRLEtLUXngY0HvbLMn' in parents", fields="nextPageToken, files(id, name)").execute()
+
+# results = service.files().list(pageSize=10, q="name='02_Pending Customer Review'", fields="nextPageToken, files(id, name)").execute()
+# results = service.files().list(pageSize=10, q="name='03_Approved For Processing'", fields="nextPageToken, files(id, name)").execute()
+
+
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    service = build('drive', 'v3', credentials=creds)
+
+
+print(creds.valid)
+#file = service.files().get(fileId=file_id, fields='parents').execute()
+
+#these are test folders
+# moveto_folder = '1qn5SRq6-o81djmXqMRJYBt1MIJo_aWdI'
+# movefrom_folder = '1euIIyv0ea7Be0UwSNztkStHF0PqOGA92'
+
+moveto_folder = '1XEXTQBX3BlAqFhMSwEARK1jPM07WWdbd'
+movefrom_folder = '1XU0nCVjQbFlK6bzRLEtLUXngY0HvbLMn'
+
+
+results = service.files().list(pageSize=200, q = "'1XU0nCVjQbFlK6bzRLEtLUXngY0HvbLMn' in parents", fields="nextPageToken, files(id, name)").execute()
+for i in range(0, len(results['files'])):
+    file_id = results['files'][i]['id']
+    file_name = results['files'][i]['name']
+    #print(file_name)
+    if file_name in approved_ids2:
+        print("MOVING")
+        print(file_name)
+        file = service.files().update(
+                fileId=file_id,
+                addParents=moveto_folder,
+                removeParents=movefrom_folder,
+                fields='id, parents'
+            ).execute()
